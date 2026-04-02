fd3411aac97afda967c7bd49a503b06b4b961c84
marcos20240230 <marcostorres2555k@gmail.com>
2026-04-01 22:59:09 -0400

    primer commit

---
+++ Script.js
@@ -0,0 +1,52 @@
+let users = [];
+
+function renderUsers() {
+  const table = document.getElementById("userTable");
+  table.innerHTML = "";
+
+  users.forEach((user, index) => {
+    table.innerHTML += `
+      <tr>
+        <td>${user.name}</td>
+        <td>${user.email}</td>
+        <td>
+          <button onclick="editUser(${index})">Editar</button>
+          <button onclick="deleteUser(${index})">Eliminar</button>
+        </td>
+      </tr>
+    `;
+  });
+}
+
+function addUser() {
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+
+  if (!name || !email) {
+    alert("Completa todos los campos");
+    return;
+  }
+
+  users.push({ name, email });
+
+  // limpiar inputs
+  document.getElementById("name").value = "";
+  document.getElementById("email").value = "";
+
+  renderUsers();
+}
+
+function deleteUser(index) {
+  users.splice(index, 1);
+  renderUsers();
+}
+
+function editUser(index) {
+  const newName = prompt("Nuevo nombre:", users[index].name);
+  const newEmail = prompt("Nuevo email:", users[index].email);
+
+  if (newName && newEmail) {
+    users[index] = { name: newName, email: newEmail };
+    renderUsers();
+  }
+}
